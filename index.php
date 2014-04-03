4c737ad565543456d238fd34ad994840fff01636
gupta-kanav <kgupta@seasiainfotech.com>
2014-04-03 11:51:42 +0530

    exit removed

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ function pg_connection_string() {
 $db = pg_connect(pg_connection_string());
 if (!$db) {
     echo "Database connection error."
-    exit;
 }
  
 // $result = pg_query($db, "SELECT statement goes here");
