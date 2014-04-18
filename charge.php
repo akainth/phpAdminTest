7112be6b41ce7f8cf1e2115b18f8a80c7f61bb7f
gupta-kanav <kgupta@seasiainfotech.com>
2014-04-18 11:22:06 +0530

    Charge.php replaced

--- charge.php
+++ charge.php
@@ -1,41 +1,72 @@
 
 
 
-
  <?php 
- require_once 'lib/Stripe.php';
+//   require_once(dirname(__FILE__) . '/config.php');
 
- Stripe::setApiKey("sk_test_6LctfFCyzLcB4RRaG12kVfiu");
+require_once 'lib/Stripe.php';
+include "config.php";
 
- $token="tok_103rxK2eVS1eWkKxO7nG4p9Z";
- if(isset($_POST)) {
-	
-	
+  $token  = $_POST['stripeToken'];
+
  try {
+ 	$customer = Stripe_Customer::create(array(
+ 			'email' => 'saggarwal1@seasiainfotech.com',
+ 			'card'  => $token
+ 	));
  	
- 	$payment = Stripe_Charge::create(array(
+ 	$charge = Stripe_Charge::create(array(
+ 			'customer' => $customer->id,
+ 			'amount'   => 5000,
+ 			'currency' => 'usd'
+ 	));
  	
- 			'amount'        => $_POST['amount'],
- 			'currency'      => $_POST['currency'],
- 			'card'          => array(
- 					'number'    => $_POST['card'],
- 					'exp_month' => $_POST['emonth'],
- 					'exp_year'  => $_POST['eyear'],
- 					'cvc'       => $_POST['eCvv']
- 			),
- 			'description'   => $_POST['description']
  	
- 	)
- 	);
- 	print_r($payment);
  	
- } catch (Stripe_Error $e) {
+ 	echo '<h1>Successfully charged $50.00!</h1>';
+ } catch (Exception $e) {
  	
- 	print_r($e);
+    echo 'ErrorMsg: ' .$e->getMessage();
  }
 
-}
- ?> 
+  
+?>
+
+
+ <?php 
+// require_once 'lib/Stripe.php';
+
+// Stripe::setApiKey("sk_test_6LctfFCyzLcB4RRaG12kVfiu");
+
+// $token="tok_103rxK2eVS1eWkKxO7nG4p9Z";
+// if(isset($_POST)) {
+	
+	
+//  try {
+ 	
+//  	$payment = Stripe_Charge::create(array(
+ 	
+//  			'amount'        => $_POST['amount'],
+//  			'currency'      => $_POST['currency'],
+//  			'card'          => array(
+//  					'number'    => $_POST['card'],
+//  					'exp_month' => $_POST['emonth'],
+//  					'exp_year'  => $_POST['eyear'],
+//  					'cvc'       => $_POST['eCvv']
+//  			),
+//  			'description'   => $_POST['description']
+ 	
+//  	)
+//  	);
+//  	print_r($payment);
+ 	
+//  } catch (Stripe_Error $e) {
+ 	
+//  	print_r($e);
+//  }
+
+// }
+// ?>
 
 
 
